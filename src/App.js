aa8683c4270754fb558f531ffa507d47c77aeadb
shantal rhobi <shantal.rohbi@student.moringaschool.com>
2024-08-19 01:17:35 +0300

    complete labs

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
 function App() {
-  return <div>Your code here</div>;
+  return (
+    <main>
+      <h1>Hi, I'm Rohbi</h1>
+      <img alt="My profile pic" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTHICAh_Q8UhD5_f1DjZJr3esByw5hsSmXwOQ&s" />
+      <h2>About Me</h2>
+      <p>
+       My biography
+      </p>
+      <div>
+        <a href="https://github.com">GitHub</a>
+        <a href="https://linkedin.com">LinkedIn</a>
+      </div>
+    </main>
+  );
 }
 
 export default App;
--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,6 +1,68 @@
-import { render, screen } from "@testing-library/react";
+import { logRoles, render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
 
-import App from "../App";
+import App from '../App';
 
 // Your tests here
+describe('App', () => {
+  test("displays a top-level heading with the text Hi, I'm _______", () => {
+    // Arrange
+    render(<App />);
+    // Act
+    const topLevelHeading = screen.getByRole('heading', {
+      name: /hi, i'm/i,
+      exact: false,
+      level: 1,
+    });
+    // Assert
+    expect(topLevelHeading).toBeInTheDocument();
+  });
+  test('displays an image with an alt attribute identifying its content', () => {
+    //Arrange
+    render(<App />);
+    //Act
+    // logRoles(render(<App />).container)
+    const image = screen.getByRole('img');
+    //Assert
+    expect(image).toHaveAttribute('alt');
+    expect(image).toHaveAttribute('src');
+  });
+  test('displays a second-level heading with the text "About Me"', () => {
+    //Arrange
+    render(<App />);
+    //Act
+    const secondLevelHeading = screen.getByRole('heading', {
+      name: /about me/i,
+      exact: false,
+      level: 2,
+    });
+    //Assert
+    expect(secondLevelHeading).toBeInTheDocument();
+  });
+  test('displays a paragraph for biography', () => {
+    //Arrange
+    render(<App />);
+    //Act
+    const biographyParagraph = screen.getByText(/my biography/i);
+    //Assert
+    expect(biographyParagraph).toBeInTheDocument();
+  });
+  test('displays two links, to Github and LinkedIn', () => {
+    //Arrange
+    render(<App />);
+    //Act
+    const githubLink = screen.getByRole('link', {
+      name: /github/i,
+    });
+    const linkedinLink = screen.getByRole('link', {
+      name: /linkedin/i,
+    });
+    //Assert
+    expect(githubLink).toBeInTheDocument()
+    expect(githubLink).toHaveAttribute('href')
+
+    expect(linkedinLink).toBeInTheDocument()
+    expect(linkedinLink).toHaveAttribute('href')
+
+  });
+});
