74551a1347b1a7931c13507e8ef86eaf1c27b3f0
shantal rhobi <shantal.rohbi@student.moringaschool.com>
2024-08-19 20:36:29 +0300

    complete labs

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 function App() {
   return (
     <main>
-      <h1>Hi, I'm Rohbi</h1>
+      <h1>Hi, I'm Shantal</h1>
       <img alt="My profile pic" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTHICAh_Q8UhD5_f1DjZJr3esByw5hsSmXwOQ&s" />
       <h2>About Me</h2>
       <p>
